e5efbc9f8254227dde34da01564b0028483f031f
ReemAletris17 <tarekreem480@gmail.com>
2026-04-18 15:13:13 +0200

    Update ProfileStack.js

--- src/navigations/ProfileStack.js
+++ src/navigations/ProfileStack.js
@@ -3,10 +3,9 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ProfileScreen from "../screens/ProfileScreen";
 import EditProfileScreen from "../screens/EditProfileScreen";
 import LearningProfileScreen from "../screens/learningProfileScreen";
-import ContactSupportScreen from "../screens/ContactSupportScreen";
+
 
 const Stack = createNativeStackNavigator();
-
 export default function ProfileStack({ signOut }) {
   return (
     <Stack.Navigator>
@@ -26,11 +25,7 @@ export default function ProfileStack({ signOut }) {
         component={LearningProfileScreen}
         options={{ headerShown: false }}
       />
-      <Stack.Screen
-        name="ContactSupport"
-        component={ContactSupportScreen}
-        options={{ headerShown: false }}
-      />
+      
     </Stack.Navigator>
   );
 }
